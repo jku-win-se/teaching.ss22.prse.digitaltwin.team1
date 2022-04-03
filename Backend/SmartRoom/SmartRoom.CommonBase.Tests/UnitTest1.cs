40ba8bce5cf25b7ec41d781eb07ca351f484311a
mue-ma <m.muehleder@gmail.com>
2022-04-03 09:22:10 +0200

    added TestProj for commonbase #6

---
+++ Backend/SmartRoom/SmartRoom.CommonBase.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace SmartRoom.CommonBase.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
