59905bc911d4a9c0ac638d1f1eb8dfbcb7867a80
mue-ma <markus.muehleder@gmail.com>
2022-04-05 14:09:55 +0200

    added generic csv read/write class #37

---
+++ Backend/SmartRoom/SmartRoom.CommonBase/Utils/GenericCSVReader.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace SmartRoom.CommonBase.Utils
+{
+    public class GenericCSVReader<T> : IDisposable where T : new()
+    {
+        List<T> _data = new List<T>();
+        private string _fileName;
+
+        private SafeHandle _safeHandle = new SafeFileHandle(IntPtr.Zero, true);
+
+        public GenericCSVReader(string fileName)
+        {
+            if (!fileName.Contains(".csv")) throw new FormatException("No CSV File!");
+            else _fileName = fileName;
+        }
+
+        public IEnumerable<T> Read()
+        {
+            IEnumerable<string> lines = File.ReadLines(_fileName);
+            string[] headers = lines.First().Split(";");
+
+            foreach (var line in lines.Skip(1))
+            {
+                var t = new T();
+                foreach (var header in headers)
+                {
+                    int i = Array.IndexOf(headers, headers.Where(h => h.Equals(header)));
+                    PropertyInfo? prop = typeof(T).GetProperty(header);
+                    Type? type = prop?.PropertyType;
+
+                    if (typeof(int).Equals(type))
+                    {
+                        prop?.SetValue(t, Convert.ToInt32(line.Split(";")[i]));
+                    }
+                    else if (typeof(DateTime).Equals(type))
+                    {
+                        prop?.SetValue(t, Convert.ToDateTime(line.Split(";")[i]));
+                    }
+                    else if (typeof(bool).Equals(type))
+                    {
+                        prop?.SetValue(t, Convert.ToBoolean(line.Split(";")[i]));
+                    }
+                    else prop?.SetValue(t, line.Split(";")[i]);
+                }
+
+                _data.Add(t);
+
+            }
+
+            return _data;
+        }
+
+        #region IDisposable Support
+        private bool _disposedValue = false; 
+
+        public void Dispose() => Dispose(true);
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    _safeHandle.Dispose();
+                }
+
+                _data = new List<T>();
+                _disposedValue = true;
+            }
+        }
+        #endregion
+    }
+}
---
+++ Backend/SmartRoom/SmartRoom.CommonBase/Utils/GenericCSVWriter.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System.Runtime.InteropServices;
+
+namespace SmartRoom.CommonBase.Utils
+{
+    public class GenericCSVWriter<T> : IDisposable
+    {
+        private IEnumerable<T> _data;
+        private SafeHandle _safeHandle = new SafeFileHandle(IntPtr.Zero, true);
+        private string? _fileName;
+        public string? FileName
+        {
+            get { return _fileName; }
+            private set 
+            {
+                if (string.IsNullOrEmpty(value)) _fileName =$"{_data.First()?.GetType().Name}_{DateTime.Now.ToShortDateString()}.csv";
+                else _fileName = value;
+            }
+        } 
+
+        public GenericCSVWriter(IEnumerable<T> data, string fileName)
+        {
+            if(_data is not null) _data = data;
+            else throw new ArgumentNullException(nameof(data));
+
+            FileName = fileName;
+        }
+
+        public string WriteToCSV()
+        {
+            string ret = $"{ Directory.GetCurrentDirectory() }\\{ _fileName}";
+            File.WriteAllLines($"{Directory.GetCurrentDirectory()}\\{_fileName}", GetDataForCSV());
+            return ret;
+        }
+
+        private string[] GetDataForCSV()
+        {
+            List<string> lines = new List<string>();
+            string headLine = "";
+            
+
+            var header = _data.First()!.GetType().GetProperties().ToArray();
+
+
+            foreach (var item in header)
+            {
+                headLine = headLine + $"{item.Name};";
+            }
+
+            lines.Add(headLine);
+
+            foreach (var obj in _data)
+            {
+                string line = "";
+                foreach (var item in header)
+                {
+                    line += $"{obj?.GetType().GetProperty(item.Name)?.GetValue(obj)};";
+                }
+                lines.Add(line);
+            }
+            return lines.ToArray();
+
+        }
+
+        #region IDisposable Support
+        private bool _disposedValue = false;
+
+        public void Dispose() => Dispose(true);
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    _safeHandle.Dispose();
+                }
+
+                _data = new List<T>();
+                _disposedValue = true;
+            }
+        }
+        #endregion
+    }
+}
