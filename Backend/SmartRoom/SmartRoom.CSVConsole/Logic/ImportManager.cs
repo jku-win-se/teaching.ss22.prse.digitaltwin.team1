9d1beb7b3c1d5bc2dab24fac1dbd55c35d96e74d
FloDo3000 <florian.dobretsberger@gmx.at>
2022-04-08 17:24:59 +0200

    improved csv-reader; initial csv-importer

---
+++ Backend/SmartRoom/SmartRoom.CSVConsole/Logic/ImportManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartRoom.CSVConsole.Logic
+{
+    internal class ImportManager
+    {
+    }
+}
