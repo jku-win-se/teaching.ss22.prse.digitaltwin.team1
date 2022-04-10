89a0e267c4375ced30a376702cc140faf524c63a
FloDo3000 <florian.dobretsberger@gmx.at>
2022-04-10 11:25:03 +0200

    Updated CSV_Base_Data #37

--- Backend/SmartRoom/SmartRoom.CSVConsole/Program.cs
+++ Backend/SmartRoom/SmartRoom.CSVConsole/Program.cs
@@ -7,11 +7,13 @@ using SmartRoom.CSVConsole.Models;
 
 
 Console.WriteLine("Hello, World!");
-IEnumerable<WindowOpen> data;
+IEnumerable<WindowOpen> windowStates;
 
 
 
 using (GenericCSVReader<WindowOpen> reader = new GenericCSVReader<WindowOpen>(@"C:\Daten\Git\teaching.ss22.prse.digitaltwin.team1\Backend\SmartRoom\SmartRoom.CSVConsole\Data\WindowOpen.csv"))
 { 
-   data = reader.Read();
-}
+   windowStates = reader.Read();
+} 
+
+//ToDo Create Props and readers for all csv
