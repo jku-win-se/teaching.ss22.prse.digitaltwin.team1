716f4589f1e643ef1a740fb5b8ef264ccfe4a253
mue-ma <m.muehleder@gmail.com>
2022-04-30 20:20:29 +0200

    Testing CD #42

---
+++ Backend/SmartRoom/SmartRoom.BaseDataService/Controllers/Test.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartRoom.BaseDataService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Test : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Test";
+        }
+    }
+}
