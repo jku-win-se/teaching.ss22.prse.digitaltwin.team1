4537a54578ccd78deb39404bb298908b5f076b28
mue-ma <m.muehleder@gmail.com>
2022-05-20 19:50:40 +0200

    imorved energysavings after tests #57

--- Backend/SmartRoom/SmartRoom.TransDataService/Logic/EnergySavingManager.cs
+++ Backend/SmartRoom/SmartRoom.TransDataService/Logic/EnergySavingManager.cs
@@ -7,8 +7,6 @@ namespace SmartRoom.TransDataService.Logic
     {
         private readonly ReadManager _readManager;
         private readonly IConfiguration _configuration;
-
-        private string _baseDataServiceURL => _configuration["Services:BaseDataService"];
         private string _dataSimulatorURL => _configuration["Services:DataSimulatorService"];
         private string _apiKey => _configuration["ApiKey"];
 
@@ -20,14 +18,16 @@ namespace SmartRoom.TransDataService.Logic
         //Energy Saving: Turn lights on if there are people in the room.
         public void TurnLightsOnPeopleInRoom(IEnumerable<IState> states)
         {
-            states.Where(s => s.Name.Equals("PeopleInRoom")).Select(s => s as MeasureState)
+            if (DateTime.Now.Hour <= 8 || DateTime.Now.Hour >= 19) 
+            {
+                states.Where(s => s.Name.Equals("PeopleInRoom")).Select(s => s as MeasureState)
                 .Where(s => s?.Value > 0 &&
-                 _readManager.GetStatesByEntityID<MeasureState>(s!.EntityRefID).GetAwaiter().GetResult().Take(2).ToArray()[1].Value == 0 &&
-                 DateTime.Now.Hour <= 8 && DateTime.Now.Hour >= 19)
+                 _readManager.GetStatesByEntityID<MeasureState>(s!.EntityRefID).GetAwaiter().GetResult().Take(2).ToArray()[1].Value == 0)
                 .ToList().ForEach(async s =>
                 {
                     await CommonBase.Utils.WebApiTrans.GetAPI<object>($"{_dataSimulatorURL}command/SetAllBianriesForRoomByEquipmentType/{s?.EntityRefID}&Light&true", _apiKey);
                 });
+            }
         }
 
         // Energy Saving: Lights should be turned off if the room is empty.
