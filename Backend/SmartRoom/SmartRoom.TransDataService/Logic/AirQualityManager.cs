68a2b789db337bb69a036de3173379b6b80f0e7a
FloDo3000 <florian.dobretsberger@gmx.at>
2022-05-25 13:08:51 +0200

    Updated AirQualityManager (Requ17) #59

--- Backend/SmartRoom/SmartRoom.TransDataService/Logic/AirQualityManager.cs
+++ Backend/SmartRoom/SmartRoom.TransDataService/Logic/AirQualityManager.cs
@@ -5,30 +5,41 @@ namespace SmartRoom.TransDataService.Logic
 {
     public class AirQualityManager
     {
+
+        
+        private readonly IConfiguration _configuration;
+        private string _dataSimulatorURL => _configuration["Services:DataSimulatorService"];
+        private string _apiKey => _configuration["ApiKey"];
+
+        public AirQualityManager(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         //Air Quality: Open window + activate fan if co2 values are > 1000 parts per million (ppm).
         public async void CheckCo2ImporveAitQuality(IEnumerable<IState> states) 
         {
             if (!states.Any()) return;
 
             await Task.Run(() =>
-            states.Where(s => s!.Name.Equals("Airquality")).Select(s => s as MeasureState).Where(s => s?.Value > 1000).ToList()
+            states.Where(s => s!.Name.Equals("Co2")).Select(s => s as MeasureState).Where(s => s?.Value > 1000).ToList()
                 .ForEach(async s =>
                 {
-                    await OpenWindowsByState(s);
-                    await RunFansByState(s);    
+                    await OpenWindowsByState(s!);
+                    await RunFansByState(s!);    
                 }
             ));
             
         }
 
-        public async Task OpenWindowsByState(IState state) 
+        public async Task OpenWindowsByState(IState s) 
         {
-            Console.WriteLine(state.Name);
+            await CommonBase.Utils.WebApiTrans.GetAPI<object>($"{_dataSimulatorURL}command/SetAllBianriesForRoomByEquipmentType/{s?.EntityRefID}&Window&true", _apiKey);
         }
 
-        public async Task RunFansByState(IState state)
+        public async Task RunFansByState(IState s)
         {
-            Console.WriteLine(state.Name);
+            await CommonBase.Utils.WebApiTrans.GetAPI<object>($"{_dataSimulatorURL}command/SetAllBianriesForRoomByEquipmentType/{s?.EntityRefID}&Ventilator&true", _apiKey);
         }
     }
 }
