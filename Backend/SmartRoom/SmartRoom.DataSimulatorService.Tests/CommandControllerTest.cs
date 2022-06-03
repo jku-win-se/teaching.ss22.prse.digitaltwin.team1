ce7f67249f1bf8ce71538c5924b07467f8e50e5b
mue-ma <m.muehleder@gmail.com>
2022-06-03 17:02:11 +0200

    added CommandController tests #53

---
+++ Backend/SmartRoom/SmartRoom.DataSimulatorService.Tests/CommandControllerTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using Moq;
+using SmartRoom.DataSimulatorService.Controllers;
+using SmartRoom.DataSimulatorService.Logic.Contracts;
+using Xunit;
+
+namespace SmartRoom.DataSimulatorService.Tests
+{
+    public class CommandControllerTest
+    {
+        [Fact]
+        public void Ctor_ValidParam_Ok()
+        {
+            var cont = new CommandController(new Mock<ISensorManager>().Object, new Mock<IHostApplicationLifetime>().Object);
+            Assert.NotNull(cont);
+        }
+
+        [Fact]
+        public void ChangeBianry_ValidParams_OkResult()
+        {
+            var cont = new CommandController(new Mock<ISensorManager>().Object, new Mock<IHostApplicationLifetime>().Object);
+            Assert.IsType<OkResult>(cont.ChangeBianry(new System.Guid(), "isTriggerd"));
+        }
+
+        [Fact]
+        public void SetAllBianriesForRoomByEquipmentType_ValidParams_OkResult()
+        {
+            var cont = new CommandController(new Mock<ISensorManager>().Object, new Mock<IHostApplicationLifetime>().Object);
+            Assert.IsType<OkResult>(cont.SetAllBianriesForRoomByEquipmentType(new System.Guid(), "isTriggerd", true));
+        }
+
+        [Fact]
+        public void StopApplication__OkResult()
+        {
+            var cont = new CommandController(new Mock<ISensorManager>().Object, new Mock<IHostApplicationLifetime>().Object);
+            Assert.IsType<OkResult>(cont.StopService());
+        }
+    }
+}
