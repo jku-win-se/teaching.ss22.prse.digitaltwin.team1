1863715b2b9543d11b26fbfe5f7c4eef981d906d
mue-ma <m.muehleder@gmail.com>
2022-06-05 13:32:32 +0200

    added BaseDataServiceContext tests #52

---
+++ Backend/SmartRoom/SmartRoom.CommonBase.Tests/BaseDataServiceContextTest.cs
@@ -0,0 +1,42 @@
+﻿using Moq;
+using SmartRoom.CommonBase.Transfer;
+using SmartRoom.CommonBase.Transfer.Contracts;
+using Xunit;
+
+namespace SmartRoom.CommonBase.Tests
+{
+    public class BaseDataServiceContextTest
+    {
+        [Fact]
+        public void Ctor_ValidParam_Ok()
+        {
+            var mock = new Mock<IServiceRoutesManager>();
+
+            var context = new BaseDataServiceContext(mock.Object);
+
+            Assert.NotNull(context);
+        }
+
+        [Fact]
+        public void GetRooms_ValidResult()
+        {
+            var mock = new Mock<IServiceRoutesManager>();
+            mock.Setup(m => m.BaseDataService).Returns("https://basedataservice.azurewebsites.net/api/");
+            mock.Setup(m => m.ApiKey).Returns("bFR9bGhOi0n0ccoEhrhsE57VrHjkJJz9");
+            var context = new BaseDataServiceContext(mock.Object);
+
+            Assert.NotNull(context.GetRooms());
+        }
+
+        [Fact]
+        public void GetRoomEquipments_ValidResult()
+        {
+            var mock = new Mock<IServiceRoutesManager>();
+            mock.Setup(m => m.BaseDataService).Returns("https://basedataservice.azurewebsites.net/api/");
+            mock.Setup(m => m.ApiKey).Returns("bFR9bGhOi0n0ccoEhrhsE57VrHjkJJz9");
+            var context = new BaseDataServiceContext(mock.Object);
+
+            Assert.NotNull(context.GetRooms());
+        }
+    }
+}
