ef4db0e65c9f3a656f5c0a38a8dd9f4eacdafc65
mue-ma <markus.muehleder@gmail.com>
2022-06-14 17:21:20 +0200

    Added Migration for UniqueIndex room(name, building) #62

---
+++ Backend/SmartRoom/SmartRoom.BaseDataService/Migrations/20220614151636_roomUniqueConstraint.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartRoom.BaseDataService.Migrations
+{
+    public partial class roomUniqueConstraint : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
